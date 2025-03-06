5f19801ba06a71eb6283c7031af88d57b88ce5cd
Nebenmor <nebenmor.anthony@gmail.com>
2025-03-06 19:27:36 +0100

    update file/folder project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/*.html",
-    "./src/js/**/*.js",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     // screens: {
     //   sm: '480px',
